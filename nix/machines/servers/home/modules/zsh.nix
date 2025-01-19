4f86ee89eec6150cdb18f4f22ff78f49e98e309a
afrianjunior <afrian.junior26@gmail.com>
2025-01-20 00:09:16 +0700

    feat(home-server): add zsh config

---
+++ nix/machines/servers/home/modules/zsh.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "jump" ];
+      theme = "robbyrussell";
+    };
+
+    initExtra = ''
+      # Additional Zsh configuration can go here
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+    '';
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+  };
+
+  # Ensure zsh is the default shell
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+}
